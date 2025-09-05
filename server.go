05baad6435123f4ba7c395844afc8e2ca71934b3
William Benfatto <william@benfatto.dev>
2025-09-05 17:20:54 -0300

    server

--- server.go
+++ server.go
@@ -2,21 +2,20 @@ package main
 
 import (
 	"context"
+	"go-grpc-server/hello"
 	"log"
 	"net"
-
-	pb "go-grpc-server/hello"
 
 	"google.golang.org/grpc"
 )
 
 type server struct {
-	pb.UnimplementedHelloServiceServer
+	hello.UnimplementedHelloServiceServer
 }
 
-func (s *server) SayHello(_ context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+func (s *server) SayHello(_ context.Context, req *hello.HelloRequest) (*hello.HelloResponse, error) {
 	log.Printf("Received request for: %s", req.Name)
-	return &pb.HelloResponse{Message: "Hello, " + req.Name + "!"}, nil
+	return &hello.HelloResponse{Message: "Hello, " + req.Name + "!"}, nil
 }
 
 func main() {
@@ -25,7 +24,7 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	grpcServer := grpc.NewServer()
-	pb.RegisterHelloServiceServer(grpcServer, &server{})
+	hello.RegisterHelloServiceServer(grpcServer, &server{})
 	log.Println("gRPC server listening on :50051")
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
